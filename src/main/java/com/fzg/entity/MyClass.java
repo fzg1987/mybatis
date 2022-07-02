ab08da7fb27931123fadf39b3e1e02bbfa88e11c
fzg1987 <beginer@yeah.net>
2022-07-03 00:55:57 +0800

    二级缓存

--- src/main/java/com/fzg/entity/MyClass.java
+++ src/main/java/com/fzg/entity/MyClass.java
@@ -2,8 +2,11 @@ package com.fzg.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 public class MyClass {
+    //public class MyClass implements Serializable {
     private Integer id;
     private String name;
 }
